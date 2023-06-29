b562f065215a5c600031447f2f77eb51196c57fa
Andrey <andreyavs.00@gmail.com>
2023-06-29 16:47:26 +0300

    feat: refactor code

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-  if (str.length === 1) return false
-  if (str.length % 2 !== 0) return false
+    if (str.length === 1) return false;
+    if (str.length % 2 !== 0) return false;
 
-  let map=[];
-
-  for(let i=0;i<bracketsConfig.length;i++) { 
-      map.push(bracketsConfig[i][0]+bracketsConfig[i][1]);
-  }
-
-  for(let y=0;y<str.length;y++){
-    for(let i=0;i<map.length;i++){
-      if(str.indexOf(map[i])<0)continue;
-      else{
-        str=str.replace(map[i], '');
-        y=0;
-        i=0;
-      } 
+    let map = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        map.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
     }
-  }
 
-  if(str.length==0)return true;
-  else return false;
+    for (let y = 0; y < str.length; y++) {
+        for (let i = 0; i < map.length; i++) {
+            if (str.indexOf(map[i]) < 0) continue;
+            else {
+                str = str.replace(map[i], '');
+                y = 0;
+                i = 0;
+            }
+        }
+    }
+
+    if (str.length == 0) return true;
+    else return false;
 }
